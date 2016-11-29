de7145e8ae3f2342e9ed9db857f8753fe3cfcf1e
casa <casa@casa-PC>
2016-11-28 19:21:12 -0500

    examen primera configuracion11

---
+++ backend/controllers/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace backend\controllers;
+
+use yii\rest\ActiveController;
+
+class UserController extends ActiveController
+{
+    public $modelClass = 'app\models\User';//aqui va la tabla de los datos q se quieran mostrar
+}
---
+++ backend/models/Persona.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "persona".
+ *
+ * @property integer $id
+ * @property string $nombre
+ * @property string $departamento
+ * @property double $saldo
+ */
+class Persona extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'persona';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['nombre', 'saldo'], 'required'],
+            [['departamento'], 'string'],
+            [['saldo'], 'number'],
+            [['nombre'], 'string', 'max' => 20],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+            'departamento' => 'Departamento',
+            'saldo' => 'Saldo',
+        ];
+    }
+}
